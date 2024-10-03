99746a339cf5a565d2f3546077e69c8ab99a4d6d
nancywang197 <108085245+nancywang197@users.noreply.github.com>
2024-10-02 22:42:47 -0500

    add springboot project structure for backend

---
+++ fivethreeone/fivethreeone/src/main/java/com/cs222/fivethreeone/FivethreeoneApplication.java
@@ -0,0 +1,13 @@
+package com.cs222.fivethreeone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivethreeoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivethreeoneApplication.class, args);
+	}
+
+}
