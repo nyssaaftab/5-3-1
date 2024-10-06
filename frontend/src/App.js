3e32545013b7f5772c1431404df944fb4d58fd6c
ejx2 <ejx2@illinois.edu>
2024-10-06 12:08:57 -0500

    Add home page setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; //add a logo later
 import './App.css';
+import React from 'react'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Our Home Page!</h1>
+        <p>Find the best restaurant for you.</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
