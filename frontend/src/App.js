f3979f3fc647851e53719be9a71fe7410bedba1f
natgaographic <natgao2@illinois.edu>
2024-10-08 00:49:16 -0500

    added roboto font and new image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import logo from './road.webp'; 
+import logo from './road.webp';
+import star from './5star.webp';
 import './App.css';
 import React from 'react'; 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={star} alt="Star" style={{ width: 'auto', height: '100px' }} />
         <h1>5-3-1</h1>
         <p>Find the best green street restaurant for you, even when you can't decide.</p>
         <img src={logo} alt="Logo" style={{ width: 'auto', height: '200px' }} />
