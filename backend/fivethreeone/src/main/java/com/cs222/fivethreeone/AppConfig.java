b43a683417dba72a7141f31704ae2cbc313982c9
nancywang197 <108085245+nancywang197@users.noreply.github.com>
2024-10-18 00:16:38 -0500

    created GooglePlacesController Test and RestTemplate Test files and tests

--- backend/fivethreeone/src/main/java/com/cs222/fivethreeone/AppConfig.java
+++ backend/fivethreeone/src/main/java/com/cs222/fivethreeone/AppConfig.java
@@ -1,8 +1,9 @@
 package com.cs222.fivethreeone;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
--- backend/fivethreeone/src/test/java/com/cs222/fivethreeone/FivethreeoneApplicationTests.java
+++ backend/fivethreeone/src/test/java/com/cs222/fivethreeone/FivethreeoneApplicationTests.java
@@ -1,26 +1,14 @@
 package com.cs222.fivethreeone;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.context.ApplicationContext;
+
+
 
 @SpringBootTest
-class FivethreeoneApplicationTests {
-    private ApplicationContext applicationContext;
-	@Test
+class FivethreeoneApplicationTests {   
+    @Test
 	void contextLoads() {
 	}
-
-	@Test
-    void testRestTemplateBean() {
-        // Retrieve the RestTemplate bean from the application context
-        RestTemplate restTemplate = applicationContext.getBean(RestTemplate.class);
-        
-        // Assert that the bean is not null and is an instance of RestTemplate
-        assertThat(restTemplate).isNotNull();
-        assertThat(restTemplate).isInstanceOf(RestTemplate.class);
-    }
 
 }
