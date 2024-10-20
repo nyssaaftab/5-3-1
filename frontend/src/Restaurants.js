b679eb28e91301c47b3da0385f067381807262a3
ejx2 <ejx2@illinois.edu>
2024-10-20 00:59:43 -0500

    Edited restaurants page

--- frontend/src/Restaurants.js
+++ frontend/src/Restaurants.js
@@ -5,6 +5,7 @@ import bangkokImage from './bangkok.jpg';
 import burritoImage from './burrito.png'
 import midsummerImage from './midsummer.webp';
 import chipotleImage from './chipotle.jpg';
+import miaImage from './miaza.jpeg';
 
 const restaurants = [
   { id: 1, name: 'Bangkok Thai', image: bangkokImage },
@@ -12,18 +13,19 @@ const restaurants = [
   { id: 3, name: 'Raising Canes', image: canesImage },
   { id: 4, name: 'Midsummer Lounge', image: midsummerImage},
   { id: 5, name: 'Chipotle', image: chipotleImage},
+  { id: 6, name: 'Mia Zas', image: miaImage},
 ];
 
 
 function Restaurants() {
   return (
-    <h1> Restaurants
-    <div className="restaurant-list">
-      {restaurants.map((restaurant) => (
-        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
-      ))}
-    </div>
-    </h1>
+    <><h1> Popular Restaurants at UIUC
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+        ))}
+      </div>
+    </h1><p> and more!</p></>
   );
 }
 
